789e3d3c5506b7a7225ed2dc41aef48087cdeea0
Artem Eroshenko <artem.eroshenko@qameta.io>
2023-12-11 23:06:25 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    java
+}
+
+tasks.withType(Wrapper::class) {
+    gradleVersion = "8.4"
+}
+
+group = "org.example.junit5"
+version = "1.0-SNAPSHOT"
+
+val allureVersion = "2.24.0"
+val aspectJVersion = "1.9.20.1"
+val kotlinVersion = "1.9.20"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.withType(JavaCompile::class) {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
+
+val agent: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = true
+}
+
+tasks.test {
+    ignoreFailures = true
+    useJUnitPlatform()
+    jvmArgs = listOf(
+        "-javaagent:${agent.singleFile}"
+    )
+}
+
+dependencies {
+    agent("org.aspectj:aspectjweaver:$aspectJVersion")
+    
+    testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
+    testImplementation("io.qameta.allure:allure-junit5")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    testImplementation("org.slf4j:slf4j-simple:2.0.9")
+}
+
+repositories {
+    mavenCentral()
+}
